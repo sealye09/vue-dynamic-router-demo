8e3c676b61d5ca5dfcf84b35d359f63b222920ba
seal <yejunhai2001@gmail.com>
2023-06-13 17:50:42 +0800

    init routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,97 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+
+export const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      label: "Home",
+    },
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: () => import("../views/Dashboard.vue"),
+    meta: {
+      label: "Dashboard",
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
+    meta: {
+      label: "Login",
+    },
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue"),
+    meta: {
+      label: "About",
+    },
+  },
+];
+
+// 动态控制的路由
+const dynamicRoutes = [
+  {
+    name: "User",
+    path: "/user",
+    component: () => import("../views/User.vue"),
+    meta: {
+      label: "User",
+      roles: ["admin", "user"],
+    },
+    children: [
+      {
+        name: "UserList",
+        path: "/list",
+        component: () => import("../views/UserList.vue"),
+        meta: {
+          label: "UserList",
+          roles: ["admin"],
+        },
+      },
+      {
+        name: "UserInfo",
+        path: "/list",
+        component: () => import("../views/UserInfo.vue"),
+        meta: {
+          label: "UserInfo",
+          roles: ["user"],
+        },
+      },
+    ],
+  },
+  {
+    path: "orders",
+    name: "UserOrders",
+    component: () => import("../views/Orders.vue"),
+    meta: {
+      label: "Orders",
+      roles: ["user"],
+    },
+    children: [
+      {
+        name: "OrderCategory",
+        path: "/category",
+        component: () => import("../views/OrderCategory.vue"),
+        meta: {
+          label: "OrderCategory",
+          roles: ["user"],
+        },
+      },
+    ],
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
